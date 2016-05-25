a20f4fe5c2ba906b8ad55238392ede523ceeb3bf
Aravindh Ramaswamy <aravindh@browserstack.com>
2016-05-25 20:07:06 +0530

    added support for safari & cleanup of misc folders to start browsers in clean state

--- browser.rb
+++ browser.rb
@@ -21,6 +21,9 @@ module Browser
         when "chrome"
           @browsertype = "Google Chrome"
           system("\"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome\" "+url.to_s+" &")
+        when "safari"
+          @browsertype = "Safari"
+          system("open -a /Applications/Safari.app/Contents/MacOS/Safari "+url.to_s)
         else
           raise ArgumentError
       end
@@ -34,16 +37,27 @@ module Browser
     def cleanup
       case @browsertype
         when "Firefox"
-          cookie_path = IO.popen("find ~/Library/ -name cookies.sqlite").read.chomp
-          if cookie_path != ""
-            system("mv \""+cookie_path+"\" /tmp/")
-          end
+          system("rm -rf ~/Library/\"Application Support\"/Firefox/*")
+          system("rm -rf ~/Library/Caches/Firefox/*")
+          system("rm -rf ~/Library/Caches/Mozilla/updates/Applications/Firefox/*")
+          system("rm -rf ~/Library/Caches/Mozilla/updates/Volumes/Firefox/*")
         when "Google Chrome"
-          cookie_path = "/Users/Aravindh/Library/\"Application Support\"/Google/Chrome/Default/Cookies*"
-          system("rm -f "+cookie_path)
-      end
+          system("rm -rf ~/Applications/\"Chrome Apps.localize\"/*")
+          system("rm -rf ~/Library/\"Application Support\"/Google/Chrome/*")
+          system("rm -rf ~/Library/Caches/Google/Chrome/*")
+        when "Safari"
+          system("rm -rf ~/Library/Caches/Metadata/Safari/*")
+          system("rm -rf ~/Library/\"Application Support\"/Spigot/Safari/*")
+          system("rm -rf ~/Library/Caches/com.apple.Safari*")
+          system("rm -rf ~/Library/Caches/com.apple.commerce.safari/*")
+          system("rm -rf ~/Library/Safari/*")
+          system("rm -rf ~/Library/\"Saved Application State\"/com.apple.Safari.savedState*")
+          system("rm -rf ~/Library/WebKit/com.apple.Safari/*")
+          system("rm -rf ~/Library/Cookies/*")
+          system("rm -rf ~/Library/Preferences/com.apple.Safari*")
+          system("rm -rf ~/Library/\"Internet Plug-ins\"/*")
+        end
     end
-    
   end
 end
 
